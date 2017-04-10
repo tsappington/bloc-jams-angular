9bd2f2775336a35529c4381aaef0804d692254b5
Ted Sappington <tedsappington@gmail.com>
2017-04-09 19:57:17 -0700

    Checkpoint 5: Controllers - completed

---
+++ app/scripts/fixtures.js
@@ -0,0 +1,49 @@
+ var albumPicasso = {
+     title: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { title: 'Blue', duration: '161.71', audioUrl: 'assets/music/blue' },
+         { title: 'Green', duration: '103.96', audioUrl: 'assets/music/green' },
+         { title: 'Red', duration: '268.45', audioUrl: 'assets/music/red' },
+         { title: 'Pink', duration: '153.14', audioUrl: 'assets/music/pink' },
+         { title: 'Magenta', duration: '374.22', audioUrl: 'assets/music/magenta' }
+     ]
+ };
+ 
+ var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21'},
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15'}
+     ]
+ };
+
+ var albumLaika = {
+     title: 'Good Looking Blues',
+     artist: 'Laika',
+     label: 'Too Pure',
+     year: '2000',
+     albumArtUrl: 'assets/images/album_covers/11.png',
+     songs: [
+         { title: 'Black Cat Bone', duration: '4:24' },
+         { title: 'Moccasin', duration: '6:01' },
+         { title: 'T. Street', duration: '3:21'},
+         { title: 'Uneasy', duration: '3:14' },
+         { title: 'Good Looking Blues', duration: '2:15'},
+         { title: 'Widows Weed', duration: '3:14' },
+         { title: 'Glory Cloud', duration: '3:44' },
+         { title: 'Go Fish', duration: '4:35' },
+         { title: 'Badtimes', duration: '4:51' },
+         { title: 'Knowing Too Little', duration: '8:27' }
+     ]
+ };
